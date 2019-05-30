9bdc8566f1b745c4299c4912237382530a64c5ab
David M <mullaned@gmail.com>
2019-05-30 16:06:59 +0100

    build control component

---
+++ src/components/Sandwich/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './BuildControls.css'
+ 
+const buildControls = (props) => {
+ 
+ <div className={classes}>
+
+ </div>
+};
+ 
+export default buildControls; 
