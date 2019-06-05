9bc7440693a35475e9dab4ea89a2207a9a5e835d
David M <mullaned@gmail.com>
2019-06-05 20:21:33 +0100

    use lifecycle hooks to improve performance

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,6 +4,16 @@ import Backdrop from '../Backdrop/Backdrop'
 import Aux from '../../../hoc/Auxilary'
 
 class Modal extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+
+  componentWillUpdate() {
+    console.log('[Modal] will update')
+  }
+
+
   render() {
     return(
       <Aux>
