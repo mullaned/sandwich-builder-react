1f2692276aadb0a02c1f175b788be219d55ac962
David M <mullaned@gmail.com>
2019-06-17 14:42:09 +0100

    build checkout container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
 import SandwichMaker from './containers/SandwichMaker/SandwichMaker'
+import Checkout from './containers/Checkout/Ckeckout'
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div>
         <Layout>
           <SandwichMaker />
+          <Checkout />
         </Layout>
       </div>
     );
---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Sandwich from '../../Sandwich/Sandwich'
+
+import Button from '../../UI/Button/Button'
+import classes from './CheckoutSummary.css'
+ 
+const checkoutSummary = (props) => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <h1>We hope you enjoy it!</h1>
+      <div style={{width: '100%', margin:'auto'}}>
+        <Sandwich ingredients={props.ingredients} />
+      </div>
+      <Button btnType="Danger" clicked>Cancel</Button>
+      <Button btnType="Success" clicked>Continue</Button>
+    </div>
+  )
+ 
+};
+ 
+export default checkoutSummary;
